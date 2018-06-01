73bf8654e864d67fbbcfe3662cd69d5b60844ef3
EdgarJRG <edgarj_rodriguezg@hotmail.com>
2018-06-01 19:49:11 -0400

    aded types to lenses and removed unused variables

--- fetchRepositories/src/fetchRepositories.spec.ts
+++ fetchRepositories/src/fetchRepositories.spec.ts
@@ -2,20 +2,19 @@ import { valuesLens, linkLens, getLinks } from './fetchRepositories'
 import { view } from 'ramda'
 
 test('valuesLens should bring the value of response', async () => {
-  const PRsReponse = require('./testData/mergedPRsResponse.json')
+  const PRsResponse = require('./testData/mergedPRsResponse.json')
   const valuesLensResponse = require('./testData/valuesLensResponse.json')
-  expect(view(valuesLens, PRsReponse)).toEqual(valuesLensResponse)
+  expect(view(valuesLens, PRsResponse)).toEqual(valuesLensResponse)
 });
 
 test('linkLens should bring the link of the PR', async () => {
   const linkLensInput = require('./testData/linkLens.input.json')
   const linkLensExpected = require('./testData/linkLens.expected.json')
-  expect(view(linkLens, linkLensInput)).toEqual(linkLensExpected)
+  expect(view<string, any>(linkLens, linkLensInput)).toEqual(linkLensExpected)
 });
 
 test('getLinks should bring the link of the PR', async () => {
-  const PRsReponse = require('./testData/mergedPRsResponse.json')
-  const linkLensExpected = require('./testData/linkLens.expected.json')
-  expect(getLinks(PRsReponse)).toMatchSnapshot()
+  const PRsResponse = require('./testData/mergedPRsResponse.json')
+  expect(getLinks(PRsResponse)).toMatchSnapshot()
 });
 
--- fetchRepositories/src/fetchRepositories.ts
+++ fetchRepositories/src/fetchRepositories.ts
@@ -1,12 +1,12 @@
-import { lensProp, lensPath, transduce, view, compose, map } from "ramda";
+import { lensProp, lensPath, view, map } from 'ramda';
 
 export class Fetcher {
 
 }
 
 export const valuesLens = lensProp('values')
-export const linkLens = lensPath(['links', 'self', 'href'])
+export const linkLens = lensPath<any, string>(['links', 'self', 'href'])
 
-export function getLinks(input) {
+export function getLinks(input): string[] {
   return map(view(linkLens), view(valuesLens, input))
 }
